d263616b370185c07aabb43620caee7003fba9f6
Jimmy Huang <jimmy@netivism.com.tw>
2017-01-12 21:11:01 +0800

    refs #18992, add qrcode and barcode support for tax receipt.

--- invpay2go/invpay2go.tpl.php
+++ invpay2go/invpay2go.tpl.php
@@ -18,16 +18,37 @@ echo $receipt['SiteName'];
 **/
 ?>
 <div id="invpay2go-receipt" class="tax-receipt invpay2go-wrapper">
-  <h3><?php print $receipt['SiteName']; ?></h3>
+  <h3><?php print $receipt['title']; ?></h3>
   <h2>電子發票證明聯</h2>
   <h3><?php print $receipt['ChineseMonth']; ?></h3>
   <h3><?php print $receipt['InvoiceNumber']; ?></h3>
   <div><?php print $receipt['CreateTime']; ?></div>
   <div>
-    <div>隨機碼：<?php print $receipt['RandomNum']; ?></div>
-    <div>總計：＄<?php print $receipt['TotalAmt']; ?></div>
+    <div class="row">
+      <div class="left">隨機碼：<?php print $receipt['RandomNum']; ?></div>
+      <div class="right">總計：＄<?php print $receipt['TotalAmt']; ?></div>
+    </div>
+    <div class="row">
+      <div class="left">賣方：<?php print $receipt['serial']; ?></div>
+      <div class="right">買方：<?php print $receipt['BuyerUBN']; ?></div>
+    </div>
     <?php if ($receipt['Barcode']) { ?><div><img src="<?php print $receipt['Barcode']; ?>" /></div><?php } ?>
-    <?php if ($receipt['QRcodeLimg']) { ?><div><img src="<?php print $receipt['QRcodeLimg']; ?>" /></div><?php } ?>
-    <?php if ($receipt['QRcodeRimg']) { ?><div><img src="<?php print $receipt['QRcodeRimg']; ?>" /></div><?php } ?>
+    <div class="row">
+      <?php if ($receipt['QRcodeLimg']) { ?><div class="left"><img src="<?php print $receipt['QRcodeLimg']; ?>" /></div><?php } ?>
+      <?php if ($receipt['QRcodeRimg']) { ?><div class="right"><img src="<?php print $receipt['QRcodeRimg']; ?>" /></div><?php } ?>
+    </div>
+    <div class="center">交易明細資料</div>
+    <div>
+      <div>發票號碼：<?php print $receipt['InvoiceNumber']; ?></div>
+      <ul>
+      <?php foreach($receipt['ItemDetail'] as $item) { ?>
+        <li><?php print $item->ItemName.'x'.$item->ItemNum ?> <?php print 'NTD $'.$item->ItemPrice; ?></li>
+      <?php } ?>
+      </ul>
+    </div>
+    <div>銷售額：<?php print $receipt['Amt']; ?></div>
+    <?php if ($receipt['TaxAmt']) { ?><div>稅額：<?php print $receipt['TaxAmt']; ?></div><?php } ?>
+    <div>總計：<?php print $receipt['TotalAmt']; ?></div>
+
   </div>
 </div>
